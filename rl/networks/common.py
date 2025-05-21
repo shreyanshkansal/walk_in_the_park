6f08836d1abd2eafe77fad4cab2834e218b90038
shreyanshkansal <kansalshreyansh@gmail.com>
2025-05-21 20:35:18 +0800

    solved jax.tree_map depreciation warning

--- rl/networks/common.py
+++ rl/networks/common.py
@@ -1,6 +1,6 @@
 import flax.linen as nn
-import jax
 from flax.core import FrozenDict
+from jax import tree_util
 
 default_init = nn.initializers.xavier_uniform
 
@@ -8,7 +8,7 @@ default_init = nn.initializers.xavier_uniform
 def soft_target_update(critic_params: FrozenDict,
                        target_critic_params: FrozenDict,
                        tau: float) -> FrozenDict:
-    new_target_params = jax.tree_map(lambda p, tp: p * tau + tp * (1 - tau),
+    new_target_params = tree_util.tree_map(lambda p, tp: p * tau + tp * (1 - tau),
                                      critic_params, target_critic_params)
 
     return new_target_params
